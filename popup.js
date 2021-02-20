92d1aefefddb8ce0686421f08d4e78d0091e3dd1
sebastian.e.azcarate <sebazc@gmail.com>
2021-02-20 13:28:56 -0300

    End of day. To be continue later.

--- popup.js
+++ popup.js
@@ -2,34 +2,69 @@ let url;
 let title;
 let node;
 
-//getURL();
+
+
+function setURL(urle) {
+    document.getElementById("comment").value = urle;
+}
+
+
+
+
+document.getElementById("name").value = getCurrentTabTitle();
 
 
 // Gets focus over add bookmark button
 document.getElementById("add_bookmark_button").focus();
 
 // Gets the url of the current tab
-//function getURL() {
-chrome.tabs.query({active: true, lastFocusedWindow: true}, tabs => {
-    url = tabs[0].url;
-});
-//}
+/*function getCurrentTabURL() {
+    let currentTabURL;
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
+        currentTabURL = tabs[0].url; // url = tabs[0].url;
+        alert(tabs[0].url)
+    });
+    return currentTabURL;
+}
+*/
+// 
 
-// Gets the title of the tab, and put it in the name field
-chrome.tabs.getSelected(null, function(tab) {
-    title = tab.title;
-    document.getElementById("name").value = title;
-});
-
-// Gets the node folder of the last bookmark, and put it in folder field
-chrome.bookmarks.getRecent(1, (bookmarks) => {
-    let parent = bookmarks[0].parentId;
-
-    chrome.bookmarks.get(parent, (folders) => {
-        node = folders[0].title;
-        document.getElementById("folder").value = node;
+let promise = new Promise((resolve, reject) => {
+    chrome.tabs.query({active: true, lastFocusedWindow: true}, (tabs) => {
+        resolve(tabs[0].url); // url = tabs[0].url;
     });
 });
+
+promise.then(setURL);
+
+
+// Gets the title of the tab, and put it in the name field
+
+function getCurrentTabTitle() {
+    let x;
+    chrome.tabs.getSelected(null, function(tab) { // gets one item
+        //title = tab.title;
+        x = tab.title;
+        // document.getElementById("name").value = title;
+    });
+    return x;
+}
+
+// Gets the node folder of the last bookmark, and put it in folder field
+
+function getLastBookmarkNodeTitle() {
+    chrome.bookmarks.getRecent(1, (bookmarks) => {
+        let parent = bookmarks[0].parentId;
+
+        chrome.bookmarks.get(parent, (folders) => {
+            node = folders[0].title;
+            document.getElementById("folder").value = node;
+        });
+    });
+}
+
+
+
 
 // Add a bookmark for the current tab
 document.getElementById("add_bookmark_button").addEventListener("click", () => {
@@ -41,10 +76,11 @@ document.getElementById("add_bookmark_button").addEventListener("click", () => {
 });
 
 // Enables remove button
-
+/*
 chrome.bookmarks.search(url, (bookmark) => {
     document.getElementById("remove_bookmark_button").disabled = false;
 });
+*/
 
 // Remove bookmark for the current tab
 document.getElementById("remove_bookmark_button").addEventListener("click", () => {
